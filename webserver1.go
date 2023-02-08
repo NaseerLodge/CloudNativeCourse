cb99c8f94ed6e1e1c59b59cb01ff7e68e7f84c76
NaseerLodge <nlodge@uncc.edu>
2023-02-07 23:00:46 -0500

    Complete

--- webserver1.go
+++ webserver1.go
@@ -32,12 +32,8 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 
 func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	fmt.Println("ITEM: ", item)
 
 	if price, ok := db[item]; ok {
-		fmt.Println("ITEM: ", item)
-		fmt.Println("PRICE: ", price)
-		fmt.Println("OK: ", ok)
 		fmt.Fprintf(w, "%s\n", price)
 	} else {
 		w.WriteHeader(http.StatusNotFound) // 404
@@ -51,11 +47,13 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 	price := req.URL.Query().Get("price")
 	priceFloat, _ := strconv.ParseFloat(price, 32)
 
-	fmt.Println("ITEM: ", item)
-	fmt.Println("PRICE: ", price)
-
-	delete(db, item)
-	db[item] = dollars(priceFloat)
+	if _, ok := db[item]; ok {
+		delete(db, item)
+		db[item] = dollars(priceFloat)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
 
 }
 
@@ -65,9 +63,6 @@ func (db database) create(w http.ResponseWriter, req *http.Request) {
 	price := req.URL.Query().Get("price")
 	priceFloat, _ := strconv.ParseFloat(price, 32)
 
-	fmt.Println("ITEM: ", item)
-	fmt.Println("PRICE: ", price)
-
 	db[item] = dollars(priceFloat)
 
 }
@@ -76,30 +71,13 @@ func (db database) delete(w http.ResponseWriter, req *http.Request) {
 
 	item := req.URL.Query().Get("item")
 
-	delete(db, item)
+	//delete(db, item)
 
-}
-
-/*
-func (db database) update(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	//convert string to float and get error status ok
-	priceStr := req.URL.Query().Get("price")
-	priceFloat, ok := strconv.ParseFloat(priceStr, 32)
-	RWLock.Lock() //Works similarly to create, but instead it checks if the item already exists
-	if _, itemExist := db[item]; itemExist {
-		if ok == nil {
-			db[item] = dollars(priceFloat) // changes the price
-			RWLock.Unlock()
-			fmt.Fprintf(w, "updated price of item %s: %s", item, dollars(priceFloat))
-
-		} else {
-			fmt.Fprintf(w, "Invalid price %s", priceStr)
-			RWLock.Unlock()
-		}
+	if _, ok := db[item]; ok {
+		delete(db, item)
 	} else {
-		fmt.Fprintf(w, "%s does not exist in list", item)
-		RWLock.Unlock()
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
 	}
+
 }
-*/
