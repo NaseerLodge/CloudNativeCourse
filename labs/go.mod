7762c7a293e988593fcac44ff0c00435b0713962
NaseerLodge <nlodge@uncc.edu>
2023-02-21 11:58:51 -0500

    still working

--- labs/go.mod
+++ labs/go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/NaseerLodge/CloudNativeCourse/labs/lab6/weather v0.0.0-20230221062616-4fc105ce2b04 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
--- labs/lab6/main.go
+++ labs/lab6/main.go
@@ -1,10 +1,10 @@
 package main
 
-import "labs/lab6/weather"
-
-//"gitlab.com/arunravindran/cloudnativecourse/lab6-external-rest-api/weather"
-
-//"labs/lab6/weather"
+import (
+	//"gitlab.com/arunravindran/cloudnativecourse/lab6-external-rest-api/weather"
+	"github.com/NaseerLodge/CloudNativeCourse/labs/lab6/weather"
+	//"labs/lab6/weather"
+)
 
 func main() {
 	weather.RunCLI()
